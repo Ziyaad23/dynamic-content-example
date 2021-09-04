5433e02b7c4c9d7abece34f6b8992dcdacf5d096
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-04 20:18:50 +0400

    [+][ADD] Using mutable state

--- app/src/main/java/com/ziyaad/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/com/ziyaad/dynamiccontentexample/MainActivity.kt
@@ -8,49 +8,55 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ziyaad.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
-
-val namesList: ArrayList<String> = arrayListOf("John", "Michael", "Paul", "Bala")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingList(namesList)
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun GreetingList(names: List<String>) {
+fun MainScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        for (name in names) {
-            Greeting(name = name)
-        }
+        GreetingList()
+    }
+}
 
-        Button(onClick = { namesList.add("Daniel") }) {
-            Text("Add new name")
-        }
+@Composable
+fun GreetingList() {
+    val greetingListState = remember {
+        mutableStateListOf<String>("John", "Anwar")
+    }
+    for (name in greetingListState) {
+        Greeting(name = name)
+    }
+
+    Button(onClick = { greetingListState.add("Daniel") }) {
+        Text("Add new name")
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", style = MaterialTheme.typography.h4)
+    Text(text = "Hello $name!", style = MaterialTheme.typography.h5)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GreetingList(namesList)
+    MainScreen()
 }
