261d3156ff64c407b6ab21c8339cc25cb4a4d2a4
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-04 19:35:32 +0400

    [+][ADD] Create composables with dynamic contents

--- app/src/main/java/com/ziyaad/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/com/ziyaad/dynamiccontentexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ziyaad.dynamiccontentexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -10,16 +11,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.ziyaad.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
 
+val namesList:List<String> = listOf("John", "Michael", "Paul", "Bala")
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            GreetingList(namesList)
         }
     }
+}
+
+@Composable
+fun GreetingList(names: List<String>){
+    Column() {
+        for(name in names){
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String){
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-
+    GreetingList(namesList)
 }
