dca9220f68924165c1a07ea950e432a641852ab4
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-04 19:53:40 +0400

    [+][ADD] Recomposition

--- app/src/main/java/com/ziyaad/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/com/ziyaad/dynamiccontentexample/MainActivity.kt
@@ -3,15 +3,20 @@ package com.ziyaad.dynamiccontentexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ziyaad.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
 
-val namesList:List<String> = listOf("John", "Michael", "Paul", "Bala")
+val namesList: ArrayList<String> = arrayListOf("John", "Michael", "Paul", "Bala")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,17 +28,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingList(names: List<String>){
-    Column() {
-        for(name in names){
+fun GreetingList(names: List<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        for (name in names) {
             Greeting(name = name)
+        }
+
+        Button(onClick = { namesList.add("Daniel") }) {
+            Text("Add new name")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String){
-    Text(text = "Hello $name!")
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", style = MaterialTheme.typography.h4)
 }
 
 @Preview(showBackground = true)
